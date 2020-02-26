a0ba4c17dc7b31c463f00969c7fa7bcf27ba9b54
komlan Sodji <duhamelgking@gmail.com>
2020-02-26 20:42:04 +0000

    first build

---
+++ precache-manifest.952848886dc7abe6d3262ce744a543b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa93a3ab09c19277d8b1e89e37c56f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2988a920c4ecb99f1c",
+    "url": "/static/css/main.1b14e4c6.chunk.css"
+  },
+  {
+    "revision": "3377c59f66567d6718eb",
+    "url": "/static/js/2.927c493e.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/static/js/2.927c493e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f2988a920c4ecb99f1c",
+    "url": "/static/js/main.f484f5b5.chunk.js"
+  },
+  {
+    "revision": "48dd0b8cd1852f12463a",
+    "url": "/static/js/runtime-main.14e991b3.js"
+  },
+  {
+    "revision": "08608dcb3f5e92e3d34a61125a87862e",
+    "url": "/static/media/profile.08608dcb.png"
+  },
+  {
+    "revision": "6a6400b177acd3859374523f0030e003",
+    "url": "/static/media/welcm.6a6400b1.svg"
+  }
+]);
